66dc09af9389a3bacd23cc2f8c3ca1dbefdb6f1a
Daniel Müller <email.daniel.ch@gmail.com>
2021-02-25 15:22:21 +0100

    dirty refactor main

--- birdspyview.py
+++ birdspyview.py
@@ -1,143 +1,175 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import pandas as pd
-from helpers import Homography, VoronoiPitch, Play, PitchImage, PitchDraw, get_table_download_link
+from helpers import (
+    Play,
+    PitchImage,
+    get_table_download_link,
+    download_data,
+    visualize_data,
+    visualize_pitch,
+)
 from pitch import FootballPitch
 
 tags = {
-    'Ball location @ pass start event -500 ms': '#000000',
-    'Direct opponent @ pass start event -500 ms': '#00ffff',
-    'Pass receiver @ pass start event -500ms': '#000000',
-    'Interception candidate @ pass start event -500ms': '#ff00ff',
-
-    'Ball location @ pass start event': '#000000',
-    'Direct opponent @ pass start event': '#a52a2a',
-    'Pass receiver @ pass start event': '#000000',
-    'Interception candidate @ pass start event': '#808080',
-
-    'Ball location @ pass end event': '#0000ff',
-    'Direct opponent @ pass end event': '#a52a2a',
-    'Pass receiver @ pass end event': '#000000',
-    'Interception candidate @ pass end event': '#008000',
-
-    'Body orientation # TODO': '#FFFFFF'
+    "Ball location @ pass start event -500 ms": "#000000",
+    "Direct opponent @ pass start event -500 ms": "#00ffff",
+    "Pass receiver @ pass start event -500ms": "#000000",
+    "Interception candidate @ pass start event -500ms": "#ff00ff",
+    "Ball location @ pass start event": "#000000",
+    "Direct opponent @ pass start event": "#a52a2a",
+    "Pass receiver @ pass start event": "#000000",
+    "Interception candidate @ pass start event": "#808080",
+    "Ball location @ pass end event": "#0000ff",
+    "Direct opponent @ pass end event": "#a52a2a",
+    "Pass receiver @ pass end event": "#000000",
+    "Interception candidate @ pass end event": "#008000",
+    "Body orientation # TODO": "#FFFFFF",
 }
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.beta_set_page_config(page_title='BirdsPyView', layout='wide')
+st.set_option("deprecation.showfileUploaderEncoding", False)
+st.beta_set_page_config(page_title="BirdsPyView", layout="wide")
 
-st.title('Upload Image or Video')
-uploaded_file = st.file_uploader("Select Image file to open:", type=["png", "jpg", "mp4"])
+st.title("Upload Image or Video")
+uploaded_file = st.file_uploader(
+    "Select Image file to open:", type=["png", "jpg", "mp4"]
+)
 pitch = FootballPitch()
 
-def download_data(dfCoords):
-    st.title('Data inspection and download')
-    st.markdown(get_table_download_link(dfCoords[['team', 'x', 'y', 'time']]), unsafe_allow_html=True)
-
-def visualize_data(dfCoords):
-    st.write('Player Coordinates:')
-    # Displays only the relevant columns
-    st.dataframe(dfCoords[['team', 'x', 'y', 'time']])
 
 if uploaded_file:
-    if uploaded_file.type == 'video/mp4':
-        play = Play(uploaded_file)
-        t = st.slider('You have uploaded a video. Choose the frame you want to process:', 0.0,60.0)
-        image = PitchImage(pitch, image=play.get_frame(t))
-    else:
-        image = PitchImage(pitch, image_bytes=uploaded_file)
+    t, snapshot = visualize_pitch(uploaded_file, pitch)
 
-    st.title('Pitch lines')
+    st.title("Pitch lines")
 
-    lines_expander = st.beta_expander('Draw pitch lines on selected image (2 horizontal lines, then 2 vertical lines)',
-                                      expanded=True)
+    lines_expander = st.beta_expander(
+        "Draw pitch lines on selected image (2 horizontal lines, then 2 vertical lines)",
+        expanded=True,
+    )
     with lines_expander:
-        col1, col2, col_, col3 = st.beta_columns([2,1,0.5,1])
+        col1, col2, col_, col3 = st.beta_columns([2, 1, 0.5, 1])
 
         with col1:
             canvas_image = st_canvas(
-                fill_color = "rgba(255, 165, 0, 0.3)", 
-                stroke_width = 2,
-                stroke_color = '#e00',
-                background_image = image.im,
-                width = image.im.width,
-                height = image.im.height,
-                drawing_mode = "line",
-                key = "canvas",
+                fill_color="rgba(255, 165, 0, 0.3)",
+                stroke_width=2,
+                stroke_color="#e00",
+                background_image=snapshot.im,
+                width=snapshot.im.width,
+                height=snapshot.im.height,
+                drawing_mode="line",
+                key="canvas",
             )
 
         with col2:
-            line_seq = ['UP','DP','RPA', 'RG']
+            line_seq = ["UP", "DP", "RPA", "RG"]
             h_line_options = list(pitch.horiz_lines.keys())
             v_line_options = list(pitch.vert_lines.keys())
 
-            hlines = [st.selectbox(f'Horizontal Line #{x+1}', h_line_options,
-                      key=f'hline {x}', index=h_line_options.index(line_seq[x]))
-                     for x in range(2)]
-            vlines = [st.selectbox(f'Vertical Line #{x+1}', v_line_options,
-                      key=f'vline {x}', index=v_line_options.index(line_seq[x+2]))
-                     for x in range(2)]
+            hlines = [
+                st.selectbox(
+                    f"Horizontal Line #{x+1}",
+                    h_line_options,
+                    key=f"hline {x}",
+                    index=h_line_options.index(line_seq[x]),
+                )
+                for x in range(2)
+            ]
+            vlines = [
+                st.selectbox(
+                    f"Vertical Line #{x+1}",
+                    v_line_options,
+                    key=f"vline {x}",
+                    index=v_line_options.index(line_seq[x + 2]),
+                )
+                for x in range(2)
+            ]
 
-        with col3: st.image('pitch.png', width=300)
+        with col3:
+            st.image("pitch.png", width=300)
 
     if canvas_image.json_data is not None:
         n_lines = len(canvas_image.json_data["objects"])
-        with col3: st.write(f'You have drawn {n_lines} lines. Use the Undo button to delete lines.')
-        if n_lines>=4:
-            image.set_info(pd.json_normalize(canvas_image.json_data["objects"]), hlines+vlines)
+        with col3:
+            st.write(
+                f"You have drawn {n_lines} lines. Use the Undo button to delete lines."
+            )
+        if n_lines >= 4:
+            snapshot.set_info(
+                pd.json_normalize(canvas_image.json_data["objects"]), hlines + vlines
+            )
 
             with lines_expander:
-                st.write('Converted image:')
-                st.image(image.conv_im)
+                st.write("Converted image:")
+                st.image(snapshot.conv_im)
 
             # Writing to dataframe
-            st.title('Players')
-            st.write('Draw rectangle over players on image. ' +
-                     'The player location is assumed to the middle of the base of the rectangle.')
+            st.title("Players")
+            st.write(
+                "Draw rectangle over players on image. "
+                + "The player location is assumed to the middle of the base of the rectangle."
+            )
 
-            p_col1, p_col2, p_col3 = st.beta_columns([2,1,1])
+            p_col1, p_col2, p_col3 = st.beta_columns([2, 1, 1])
 
             # Dropdown boxes
             with p_col2:
                 team_color = st.selectbox("Select Player: ", list(tags.keys()))
-                stroke_color=tags[team_color]
-                edit = st.checkbox('Edit mode (move selection boxes)')
-                update = st.button('Update data')
-                original = True #st.checkbox('Select on original image', value=True)       
-      
-            image2 = image.get_image(original)
+                stroke_color = tags[team_color]
+                edit = st.checkbox("Edit mode (move selection boxes)")
+                update = st.button("Update data")
+                original = True  # st.checkbox('Select on original image', value=True)
+
+            image2 = snapshot.get_image(original)
             height2 = image2.height
             width2 = image2.width
             with p_col1:
                 canvas_converted = st_canvas(
-                    fill_color = "rgba(255, 165, 0, 0.3)",
-                    stroke_width = 2,
-                    stroke_color = stroke_color,
-                    background_image = image2,
-                    drawing_mode = "transform" if edit else "rect",
-                    update_streamlit = update,
-                    height = height2,
-                    width = width2,
+                    fill_color="rgba(255, 165, 0, 0.3)",
+                    stroke_width=2,
+                    stroke_color=stroke_color,
+                    background_image=image2,
+                    drawing_mode="transform" if edit else "rect",
+                    update_streamlit=update,
+                    height=height2,
+                    width=width2,
                     key="canvas2",
                 )
 
             if canvas_converted.json_data is not None:
-                if len(canvas_converted.json_data["objects"])>0:
+                if len(canvas_converted.json_data["objects"]) > 0:
                     dfCoords = pd.json_normalize(canvas_converted.json_data["objects"])
                     if original:
-                        dfCoords['x'] = (dfCoords['left']+(dfCoords['width']*dfCoords['scaleX'])/2)
-                        dfCoords['y'] = (dfCoords['top']+dfCoords['height']*dfCoords['scaleY'])
-                        dfCoords[['x', 'y']] = image.h.apply_to_points(dfCoords[['x', 'y']].values)
+                        dfCoords["x"] = (
+                            dfCoords["left"]
+                            + (dfCoords["width"] * dfCoords["scaleX"]) / 2
+                        )
+                        dfCoords["y"] = (
+                            dfCoords["top"] + dfCoords["height"] * dfCoords["scaleY"]
+                        )
+                        dfCoords[["x", "y"]] = snapshot.h.apply_to_points(
+                            dfCoords[["x", "y"]].values
+                        )
                     else:
-                        dfCoords['x'] = (dfCoords['left']+dfCoords['width']*dfCoords['scaleX'])
-                        dfCoords['y'] = (dfCoords['top']+dfCoords['height']*dfCoords['scaleY'])
-                    dfCoords[['x', 'y']] = dfCoords[['x', 'y']]/image.h.coord_converter
-                    dfCoords['team'] = dfCoords.apply(lambda x: {code: color for color,code in tags.items()}.get(x['stroke']),
-                                                      axis=1)
-                    dfCoords['time'] = t
+                        dfCoords["x"] = (
+                            dfCoords["left"] + dfCoords["width"] * dfCoords["scaleX"]
+                        )
+                        dfCoords["y"] = (
+                            dfCoords["top"] + dfCoords["height"] * dfCoords["scaleY"]
+                        )
+                    dfCoords[["x", "y"]] = (
+                        dfCoords[["x", "y"]] / snapshot.h.coord_converter
+                    )
+                    dfCoords["team"] = dfCoords.apply(
+                        lambda x: {code: color for color, code in tags.items()}.get(
+                            x["stroke"]
+                        ),
+                        axis=1,
+                    )
+                    dfCoords["time"] = t
 
 
-if 'dfCoords' in globals():
+if "dfCoords" in globals():
     download_data(dfCoords)
     visualize_data(dfCoords)
 
--- helpers.py
+++ helpers.py
@@ -8,7 +8,37 @@ import streamlit as st
 import tempfile
 import base64
 
-class Homography():
+
+def download_data(dfCoords):
+    st.title("Data inspection and download")
+    st.markdown(
+        get_table_download_link(dfCoords[["team", "x", "y", "time"]]),
+        unsafe_allow_html=True,
+    )
+
+
+def visualize_data(dfCoords):
+    st.write("Player Coordinates:")
+    # Displays only the relevant columns
+    st.dataframe(dfCoords[["team", "x", "y", "time"]])
+
+
+def visualize_pitch(uploaded_file, pitch):
+    if uploaded_file.type == "video/mp4":
+        play = Play(uploaded_file)
+        t = st.slider(
+            "You have uploaded a video. Choose the frame you want to process:",
+            0.0,
+            60.0,
+        )
+        snapshot = PitchImage(pitch, image=play.get_frame(t))
+    else:
+        snapshot = PitchImage(pitch, image_bytes=uploaded_file)
+        t = 0.0
+    return t, snapshot
+
+
+class Homography:
     def __init__(self, pts_src, pts_dst):
         self.pts_src = np.array(pts_src)
         self.pts_dst = np.array(pts_dst)
@@ -16,7 +46,7 @@ class Homography():
         self.im_size = (525, 340)
         self.im_width = self.im_size[0]
         self.im_heigth = self.im_size[1]
-        self.coord_converter = np.array(self.im_size)/100
+        self.coord_converter = np.array(self.im_size) / 100
 
     def apply_to_image(self, image):
         """Applies homography to provided image.
@@ -42,40 +72,55 @@ class Homography():
         """
         h = np.linalg.inv(self.h) if inverse else self.h
         _points = np.hstack([points, np.ones((len(points), 1))])
-        _converted_points = np.dot(h,_points.T)
-        points = _converted_points/_converted_points[2]
+        _converted_points = np.dot(h, _points.T)
+        points = _converted_points / _converted_points[2]
         return points[:2].T
 
-class VoronoiPitch():
+
+class VoronoiPitch:
     def __init__(self, df):
         self.vor, self.df = calculate_voronoi(df)
 
     def get_regions(self):
-        return [index for index, region in enumerate(self.vor.regions) if (not -1 in region) and (len(region)>0)]
-    
+        return [
+            index
+            for index, region in enumerate(self.vor.regions)
+            if (not -1 in region) and (len(region) > 0)
+        ]
+
     def get_points_region(self, region):
         return np.vstack([self.vor.vertices[i] for i in self.vor.regions[region]])
-    
+
     def get_color_region(self, region):
-        return self.df[self.df['region']==region]['team'].values[0]
+        return self.df[self.df["region"] == region]["team"].values[0]
 
     def get_voronoi_polygons(self, image, original=True):
-        return [{'polygon': get_polygon(self.get_points_region(region)*image.h.coord_converter, image, original),
-                 'color': self.get_color_region(region)}
-                for region in self.get_regions()]
+        return [
+            {
+                "polygon": get_polygon(
+                    self.get_points_region(region) * image.h.coord_converter,
+                    image,
+                    original,
+                ),
+                "color": self.get_color_region(region),
+            }
+            for region in self.get_regions()
+        ]
 
-class Play():
+
+class Play:
     def __init__(self, uploaded_video):
         with tempfile.NamedTemporaryFile() as fp:
             fp.write(uploaded_video.getvalue())
             self.video = cv2.VideoCapture(fp.name)
 
     def get_frame(self, t):
-        self.video.set(cv2.CAP_PROP_POS_MSEC, t*1000)
-        success,img = self.video.read()
+        self.video.set(cv2.CAP_PROP_POS_MSEC, t * 1000)
+        success, img = self.video.read()
         return img
 
-class PitchImage():
+
+class PitchImage:
     def __init__(self, pitch, image=None, image_bytes=None, width=600):
         if image is not None:
             im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -85,17 +130,19 @@ class PitchImage():
         self.pitch = pitch
 
     def resize(self, im, width):
-        im = im.resize((width, int(width*im.height/im.width)))
+        im = im.resize((width, int(width * im.height / im.width)))
         return im
-    
+
     def set_info(self, df, lines):
-        df['line'] = lines
-        df['y1_line'] = df['top']+df['y1']
-        df['y2_line'] = df['top']+df['y2']
-        df['x1_line'] = df['left']+df['x1']
-        df['x2_line'] = df['left']+df['x2']
-        df['slope'], df['intercept'] = get_si_from_coords(df[['x1_line', 'y1_line', 'x2_line', 'y2_line']].values)
-        df = df.set_index('line')
+        df["line"] = lines
+        df["y1_line"] = df["top"] + df["y1"]
+        df["y2_line"] = df["top"] + df["y2"]
+        df["x1_line"] = df["left"] + df["x1"]
+        df["x2_line"] = df["left"] + df["x2"]
+        df["slope"], df["intercept"] = get_si_from_coords(
+            df[["x1_line", "y1_line", "x2_line", "y2_line"]].values
+        )
+        df = df.set_index("line")
         self.df = df
         self.lines = lines
         self.h = Homography(*self.get_intersections())
@@ -105,8 +152,13 @@ class PitchImage():
         lines = self.lines
         vertical_lines = [x for x in lines if x in self.pitch.vert_lines]
         horizontal_lines = [x for x in lines if x in self.pitch.horiz_lines]
-        intersections = {'_'.join([v, h]): line_intersect(self.df.loc[v, ['slope', 'intercept']], self.df.loc[h, ['slope', 'intercept']])
-                            for v,h in product(vertical_lines, horizontal_lines)}
+        intersections = {
+            "_".join([v, h]): line_intersect(
+                self.df.loc[v, ["slope", "intercept"]],
+                self.df.loc[h, ["slope", "intercept"]],
+            )
+            for v, h in product(vertical_lines, horizontal_lines)
+        }
 
         pts_src = list(intersections.values())
         pts_dst = [self.pitch.get_intersections()[x] for x in intersections]
@@ -117,43 +169,62 @@ class PitchImage():
         return self.im if original else self.conv_im
 
     def get_pitch_coords(self):
-        return ((0,0), (0,self.h.im_heigth), (self.h.im_width,self.h.im_heigth), (self.h.im_width,0))
+        return (
+            (0, 0),
+            (0, self.h.im_heigth),
+            (self.h.im_width, self.h.im_heigth),
+            (self.h.im_width, 0),
+        )
 
     def get_camera_coords(self):
-        return self.h.apply_to_points(((0,0), (0,self.im.height), (self.im.width, self.im.height), (self.im.width,0)))
+        return self.h.apply_to_points(
+            (
+                (0, 0),
+                (0, self.im.height),
+                (self.im.width, self.im.height),
+                (self.im.width, 0),
+            )
+        )
 
 
-class PitchDraw():
+class PitchDraw:
     def __init__(self, pitch_image, original=True):
         self.base_im = pitch_image.get_image(original).copy()
-        self.draw_im = Image.new('RGBA', self.base_im.size, (0,0,0,0))
-        self.draw = ImageDraw.Draw(self.draw_im, mode='RGBA')
+        self.draw_im = Image.new("RGBA", self.base_im.size, (0, 0, 0, 0))
+        self.draw = ImageDraw.Draw(self.draw_im, mode="RGBA")
         self.original = original
         self.h = pitch_image.h
 
-    def draw_polygon(self, polygon, color, outline='gray'):
-        self.draw.polygon(list(tuple(point) for point in polygon.tolist()), fill=color, outline=outline)
+    def draw_polygon(self, polygon, color, outline="gray"):
+        self.draw.polygon(
+            list(tuple(point) for point in polygon.tolist()),
+            fill=color,
+            outline=outline,
+        )
 
     def draw_voronoi(self, voronoi, image, opacity):
         for pol in voronoi.get_voronoi_polygons(image, self.original):
-            if pol['polygon'] is not None:
-                fill_color=get_rgba(pol['color'], opacity)
-                self.draw_polygon(pol['polygon'], fill_color)
+            if pol["polygon"] is not None:
+                fill_color = get_rgba(pol["color"], opacity)
+                self.draw_polygon(pol["polygon"], fill_color)
 
     def draw_circle(self, xy, color, size=1, opacity=255, outline=None):
         center = Point(*xy)
-        scaler = self.h.coord_converter/self.h.coord_converter.sum()
+        scaler = self.h.coord_converter / self.h.coord_converter.sum()
         circle = scale(center.buffer(size), *reversed(scaler))
         if self.original:
-            points = self.h.apply_to_points(np.vstack(circle.exterior.xy).T*self.h.coord_converter, inverse=True)
+            points = self.h.apply_to_points(
+                np.vstack(circle.exterior.xy).T * self.h.coord_converter, inverse=True
+            )
         else:
-            points = np.vstack(circle.exterior.xy).T*self.h.coord_converter
+            points = np.vstack(circle.exterior.xy).T * self.h.coord_converter
         fill_color = get_rgba(color, opacity)
-        if outline is None: outline = color
+        if outline is None:
+            outline = color
         self.draw_polygon(points, fill_color, outline)
 
     def draw_text(self, xy, string, color):
-        xy = xy*self.h.coord_converter
+        xy = xy * self.h.coord_converter
         font = ImageFont.load_default()
         if self.original:
             xy = self.h.apply_to_points([xy], inverse=True)[0]
@@ -161,7 +232,9 @@ class PitchDraw():
 
     def compose_image(self, sensitivity=25):
         pitch_mask = get_edge_img(self.base_im, sensitivity=sensitivity)
-        self.draw_im.putalpha(Image.fromarray(np.minimum(pitch_mask, np.array(self.draw_im.split()[-1]))))
+        self.draw_im.putalpha(
+            Image.fromarray(np.minimum(pitch_mask, np.array(self.draw_im.split()[-1])))
+        )
         return Image.alpha_composite(self.base_im.convert("RGBA"), self.draw_im)
 
 
@@ -172,53 +245,65 @@ def line_intersect(si1, si2):
         return None
     x = (b2 - b1) / (m1 - m2)
     y = m1 * x + b1
-    return x,y
+    return x, y
+
 
 def get_si_from_coords(lines):
     x1, y1, x2, y2 = lines.T
-    slope = (y2-y1) / (x2-x1)
-    intercept = y2-slope*x2
+    slope = (y2 - y1) / (x2 - x1)
+    intercept = y2 - slope * x2
     return slope, intercept
+
 
 def calculate_voronoi(df):
     from scipy.spatial import Voronoi
-    values = np.vstack((df[['x', 'y']].values,
-                        [-1000,-1000],
-                        [+1000,+1000],
-                        [+1000,-1000],
-                        [-1000,+1000]
-                       ))
+
+    values = np.vstack(
+        (
+            df[["x", "y"]].values,
+            [-1000, -1000],
+            [+1000, +1000],
+            [+1000, -1000],
+            [-1000, +1000],
+        )
+    )
     vor = Voronoi(values)
-    df['region'] = vor.point_region[:-4]
+    df["region"] = vor.point_region[:-4]
     return vor, df
+
 
 def get_polygon(points, image, convert):
     base_polygon = Polygon(points.tolist())
     pitch_polygon = Polygon(image.get_pitch_coords())
     camera_polygon = Polygon(image.get_camera_coords()).convex_hull
     polygon = camera_polygon.intersection(pitch_polygon).intersection(base_polygon)
-    if polygon.area>0:
+    if polygon.area > 0:
         if convert:
-            polygon = image.h.apply_to_points(np.vstack(polygon.exterior.xy).T, inverse=True)
+            polygon = image.h.apply_to_points(
+                np.vstack(polygon.exterior.xy).T, inverse=True
+            )
         else:
             polygon = np.vstack(polygon.exterior.xy).T
         return polygon
     else:
         return None
-    
+
+
 def get_edge_img(img, sensitivity=25):
     hsv_img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2HSV)
-    hues = hsv_img[:,:,0]
-    median_hue = np.median(hues[hues>1])
+    hues = hsv_img[:, :, 0]
+    median_hue = np.median(hues[hues > 1])
     min_filter = np.array([median_hue - sensitivity, 20, 0])
     max_filter = np.array([median_hue + sensitivity, 255, 255])
 
     mask = cv2.inRange(hsv_img, min_filter, max_filter)
     return mask
 
+
 def get_rgba(color, alpha=255):
     color = ImageColor.getrgb(color)
-    return color+(alpha,)
+    return color + (alpha,)
+
 
 def get_table_download_link(df):
     """Generates a link allowing the data in a given panda dataframe to be downloaded
@@ -226,6 +311,8 @@ def get_table_download_link(df):
     out: href string
     """
     csv = df.to_csv(index=True)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(
+        csv.encode()
+    ).decode()  # some strings <-> bytes conversions necessary here
     href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download csv file</a>'
     return href
